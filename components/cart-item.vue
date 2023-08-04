659481e20d40e1f5520c19da4a371e9872401dd8
George <georgecklei@gmail.com>
2023-08-03 22:30:33 -0700

    I will test and debug this

---
+++ components/cart-item.vue
@@ -0,0 +1,35 @@
+<style>
+
+</style>
+
+<script setup lang="ts">
+import { SOrderLineItem } from "util/types/CartUtil";
+import { Category, Item } from "util/types/ShopUtil";
+const props = defineProps<{item: SOrderLineItem }>();
+
+async function removeFromCart(itemId: any) {
+  console.log(itemId)
+  const { data: responseData } = await useFetch("api/item/remove-from-cart.ts", {
+    method: "post",
+    body: {
+      itemId: itemId,
+      cartId: useCookie("cart")
+    },
+  });
+}
+</script>
+
+<template>
+<div class="cart-items" :style="{ backgroundColor: '#' + props.item.categoryColor }">
+            <div class="left">
+            <image src="$this->image"/>
+            </div>
+            <div class="right">
+                <h2>{{ props.item.categoryName }}</h2>
+                <h4>{{ props.item.name }}</h4>
+                <h4> {{ props.item.totalMoney }} CAD</h4>
+                <button @click="removeFromCart(props.item.uid)" id="submit" class="add-cart">
+                        Remove from Cart </button>
+            </div>
+        </div>
+</template>
---
+++ server/api/item/list-cart.ts
@@ -0,0 +1,92 @@
+import { CatalogItem, CatalogObject, UpdateOrderRequest } from "square";
+import { SawkakhugSquareAPI } from "../../../util/types/ApiUtil";
+import {CartUtil,  SCart,  SOrderLineItem } from "../../../util/types/CartUtil";
+import { CategoryFinder } from "../categories";
+import ShopUtil, { ItemPhoto } from "../../../util/types/ShopUtil";
+import SuperJSON from "superjson";
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event);
+  
+    if (body.orderId == null){
+        
+    }
+  
+    let currOrder = await SawkakhugSquareAPI.getInstance()
+                              .ordersApi.retrieveOrder(body.orderId)
+                              .then((v) => v.result.order);
+    
+    if (currOrder == null || currOrder.lineItems == undefined || currOrder.lineItems.length < 1){
+        // return 
+    }
+
+    let lineItems : SOrderLineItem [] = currOrder!.lineItems!.map(v => CartUtil.createOrderLineItem(v));
+
+    if (lineItems === undefined) return;
+
+    let varItems = await SawkakhugSquareAPI.getInstance()
+                                .catalogApi.batchRetrieveCatalogObjects({
+                                    objectIds: lineItems.map(v => v.varId)
+                                });
+    
+    if (varItems.statusCode != 200) return;
+    var soldOutVarIds : string [] = [], forSaleVarIds : string [] = [], itemIds : string [] = [];
+    varItems.result.objects?.forEach((v) =>{
+        var override = v.itemVariationData?.locationOverrides;
+        if (override == null || override == undefined) return;
+        if (override[0].soldOut == true) {
+            soldOutVarIds.push(v.id)
+        }else{
+            forSaleVarIds.push(v.id);
+        }
+        itemIds.push(v.itemVariationData?.itemId!);
+    })
+
+    let items = await SawkakhugSquareAPI.getInstance().catalogApi.batchRetrieveCatalogObjects({
+        objectIds: itemIds
+    })
+
+    let mapVariationToItem : Map<string, CatalogObject> = new Map<string, CatalogObject>();
+
+    if (items.statusCode == 200) {
+        items.result.objects?.forEach(v => {
+            v.itemData?.variations?.forEach(u =>{
+                mapVariationToItem.set(u.id, v)
+            })
+        })
+    }
+
+    let mapVariationToPhoto : Map<string, string> = new Map<string, string>();    
+    let photoUrlMap : Map<string, string> = new Map<string, string>();    
+    lineItems.forEach(v => {
+        let category = CategoryFinder.find(mapVariationToItem.get(v.varId)?.itemData?.categoryId)
+        v.categoryColor = category?.color;
+        v.categoryName = category?.name;
+        mapVariationToPhoto.set(v.varId, mapVariationToItem.get(v.varId)!.itemData!.imageIds![0]);
+    })
+
+    let photos = await SawkakhugSquareAPI.getInstance().catalogApi.batchRetrieveCatalogObjects({
+        objectIds: [...mapVariationToPhoto.values()]
+    });
+
+    if (photos.statusCode === 200) {
+        photos.result.objects?.forEach((pic) => photoUrlMap.set(pic.id, pic.imageData?.url as string));
+        lineItems.forEach((item) => item.photo = mapVariationToPhoto.get(item.varId));
+    }
+
+    let removed = await SawkakhugSquareAPI.getInstance().ordersApi.updateOrder(currOrder!.id!, {
+        order: currOrder,
+        fieldsToClear: soldOutVarIds
+    });
+
+    let output : SCart = {
+        id: currOrder?.id!,
+        locationId: currOrder?.locationId!,
+        items: lineItems.filter(v => forSaleVarIds.includes(v.varId)),
+        soldOut: lineItems.filter(v => soldOutVarIds.includes(v.varId)),
+        totalPrice: Number(removed.result.order?.totalMoney?.amount) / 100.00
+    }
+    
+    return SuperJSON.stringify(output) as unknown as typeof output;
+  });
+  
